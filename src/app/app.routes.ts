95cb393fefda68ee8ce185705acff4bc3c513b91
rewanabdelaziz <rewanabdelaziz9634@gmail.com>
2024-12-09 18:46:26 +0200

    donate and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { DonateComponent } from '../components/donate/donate.component';
+import { HomeComponent } from '../components/home/home.component';
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'donate', component: DonateComponent },
+  // { path: 'about', loadComponent: () => import('../components/donate/donate.component').then(m => m.DonateComponent) },
+];
--- src/components/navbar/navbar.component.ts
+++ src/components/navbar/navbar.component.ts
@@ -1,8 +1,9 @@
+import { RouterOutlet , RouterLinkActive , RouterLink} from '@angular/router';
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-navbar',
   standalone: true,
+  imports: [RouterOutlet,RouterLinkActive,RouterLink],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
