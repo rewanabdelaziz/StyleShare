397e628720f256f120424cff55a525a8d811082e
rewanabdelaziz <rewanabdelaziz9634@gmail.com>
2024-12-10 18:20:43 +0200

    add about and contact us

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { DonateComponent } from '../components/donate/donate.component';
 import { HomeComponent } from '../components/home/home.component';
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'donate', component: DonateComponent },
-  // { path: 'about', loadComponent: () => import('../components/donate/donate.component').then(m => m.DonateComponent) },
+  // { path: 'donate', component: DonateComponent },
+  { path: 'donate', loadComponent: () => import('../components/donate/donate.component').then(m => m.DonateComponent) },
+  { path: 'about', loadComponent: () => import('../components/about/about.component').then(m => m.AboutComponent) },
+  { path: 'contactUs', loadComponent: () => import('../components/contact-us/contact-us.component').then(m => m.ContactUsComponent) },
 ];
